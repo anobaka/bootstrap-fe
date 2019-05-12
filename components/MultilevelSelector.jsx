0846ed80513ac24268d51fb7416223e5c00d7c9b
Anobaka <anobaka@anobaka.com>
2019-05-12 22:25:02 +0800

    update

--- components/MultilevelSelector.jsx
+++ components/MultilevelSelector.jsx
@@ -13,7 +13,11 @@ export default class MultilevelSelector extends Component {
   }
 
   componentDidMount() {
-    this.props.getRootDataList().then(list => {
+    this.getInitDataSource(this.props.value);
+  }
+
+  getInitDataSource = (value) => {
+    this.props.getInitDataSource(value).then(list => {
       this.setState({
         dataSource: convertTreeDataResponse(list)
       });
@@ -22,23 +26,40 @@ export default class MultilevelSelector extends Component {
 
   getNextLevelDataList = (data) => {
     return new Promise(resolve => {
-      this.props.getNextLevelDataList(data.value).then(list => {
-        const { dataSource } = this.state;
-        const item = findItemByPos(dataSource, data.pos);
-        item.children = convertTreeDataResponse(list);
-        this.setState({
-          dataSource
-        }, resolve);
-      });
+      const { dataSource } = this.state;
+      const item = findItemByPos(dataSource, data.pos);
+      if (!item.children) {
+        this.props.getNextLevelDataList(data.value).then(list => {
+          item.children = convertTreeDataResponse(list);
+          this.setState({
+            dataSource
+          }, resolve);
+        });
+      } else {
+        resolve();
+      }
     });
+  }
+
+  componentWillReceiveProps(props) {
+    // console.log('new props: ', props, 'old props: ', this.props);
+    if (!this.props.value && props.value) {
+      this.getInitDataSource(props.value);
+    }
   }
 
   render() {
     const { dataSource } = this.state;
-    const { getRootDataList, getNextLevelDataList, ...props } = this.props;
+    const { getInitDataSource, getNextLevelDataList, value, ...props } = this.props;
+    let v = value;
+    if (v) {
+      v = v.toString();
+    }
+    // console.log('rerender:', v);
     return (
       <CascaderSelect
         {...props}
+        value={v}
         dataSource={dataSource}
         loadData={this.getNextLevelDataList}
       />
